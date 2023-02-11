9d978d4429a2aca46ed61818bb5b02271796ddbd
cichan <6684177pt@gmail.com>
2023-02-12 00:18:49 +0300

    small update

--- sem5-lab4/src/base/Solution.java
+++ sem5-lab4/src/base/Solution.java
@@ -4,22 +4,11 @@ import java.util.List;
 
 public class Solution {
 	public static void main(String[] args) {
-		double step = 0.1;
-		
-		System.out.println("Решение явной РС с шагом:\nh = tau = " + step);
-		List<Double> y = Solver.solve(step, step);
-		System.out.println("Норма: " + Solver.norm1(step, y) + '\n');
-		
-		System.out.println("Решение явной РС с шагом:\nh = " + step + ", tau = " + step*step / 2);
-		y = Solver.solve(step, step*step / 2);
-		System.out.println("Норма: " + Solver.norm1(step, y) + '\n');
-		
-		System.out.println("Решение чисто неявной РС с шагом:\nh = tau = " + step);
-		y = Solver.solve(step, step);
-		System.out.println("Норма: " + Solver.norm1(step, y) + '\n');
-		
-		System.out.println("Решение схемы Кранка-Николсона с шагом:\nh = tau = " + step);
-		y = Solver.solve(step, step);
-		System.out.println("Норма: " + Solver.norm1(step, y) + '\n');
+		double h = 0.01;
+		System.out.println("Решение с шагом:\nh = " +  h);
+		List<Double> yh = Solver.solve(h);
+		System.out.println("Норма №1: " + Solver.norm1(h, yh));
+		List<Double> y2h = Solver.solve(2 * h);
+		System.out.println("Норма №2: " + Solver.norm2(yh, y2h));
 	}
 }
--- sem5-lab4/src/base/Solver.java
+++ sem5-lab4/src/base/Solver.java
@@ -1,13 +1,97 @@
 package base;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solver {
-	public static List<Double> solve(double h, double tau) {
-		return null;
+	private static final double a = 0.5;
+	private static final double b = 1;
+	
+	private static double u(double x) {
+		return 2 / (x*x + 1);
 	}
 	
+	private static double a(double h, double x) {
+		return 3 * x / (2 * h * (x*x + 1)) - 1 / (h*h);
+	}
+	
+	private static double c(double h, double x) {
+		return -2 * (1 / (h*h) + 1 / (x*x + 1));
+	} 
+	
+	private static double b(double h, double x) {
+		return  -3 * x / (2 * h * (x*x + 1)) - 1 / (h*h);
+	}
+	
+	private static double f(double h, double x) {
+		return -4 * (x*x + 2) / ((x*x + 1) * (x*x + 1) * (x*x + 1));
+	}
+
+	private static List<Double> constructAlpha(double h) {
+		List<Double> alpha = new LinkedList<>();
+		alpha.add(0.0);
+		double x = a + h;
+		int N = (int) ((b - a) / h);
+		for(int i = 1; i < N; i++, x += h) {
+			double alphai = b(h, x) / (c(h, x)- a(h, x) * alpha.get(i - 1));
+			alpha.add(alphai);
+		}
+		return alpha;
+	}
+	
+	private static List<Double> constructBeta(double h, List<Double> alpha) {
+		List<Double> beta = new LinkedList<>();
+		beta.add(1.6);
+		double x = a + h;
+		int N = (int) ((b - a) / h);
+		int i = 1;
+		for(; i < N; i++, x += h) {
+			double betai = (f(h, x) + a(h, x) * beta.get(i - 1)) / (c(h, x) - a(h, x) * alpha.get(i - 1));
+			beta.add(betai);
+		}
+		double fN = 0.5 + 9 * h / 8;
+		double aN = -1.5 - 13 * h / 8;
+		double cN = 1;
+		double betaN = (fN + aN * beta.get(i - 1)) / (cN - aN * alpha.get(i - 1));
+		beta.add(betaN);
+		return beta;
+	}
+	
+	private static List<Double> constuctAnswer(List<Double> alpha, List<Double> beta) {
+		LinkedList<Double> answer = new LinkedList<>();
+		int i = beta.size() - 1;
+		answer.push(beta.get(i--));
+		for(; i >= 0; i--) {
+			double yi = alpha.get(i) * answer.get(0) + beta.get(i);
+			answer.push(yi);
+		}
+		return answer;
+	}
+	
+	public static List<Double> solve(double h) {
+		List<Double> alpha = constructAlpha(h);
+		List<Double> beta = constructBeta(h, alpha);	
+		
+		return constuctAnswer(alpha, beta);
+	}
+
 	public static double norm1(double h, List<Double> y) {
-		return 0.0;
+		double norm = 0, x = a;
+		for (double yi : y) {
+			norm = max(norm, abs(yi - u(x)));
+			x += h;
+		}
+		return norm;
+	}
+	
+	public static double norm2(List<Double> doubleVector, List<Double> vector) {
+		double norm  = 0;
+		for(int i = 0; i < vector.size(); i++) {
+			norm = max(norm, abs(doubleVector.get(2*i) - vector.get(i)));
+		}
+		return norm / 3;
 	}
 }
